198ed42c750ec29dbbeb4a576bf98439fec31e4d
Atit <atit.rungmekharat@joingotu.com>
2025-05-05 16:47:46 +0000

    fix: providing credentials via environment variables

--- variables.tf
+++ variables.tf
@@ -1,19 +1,3 @@
-# variable "aws_access_key" {
-#   description = "AWS access key"
-#   type        = string
-# }
-
-# variable "aws_secret_key" {
-#   description = "AWS secret key"
-#   type        = string
-# }
-
-# variable "aws_token" {
-#   description = "AWS token"
-#   type        = string
-#   default     = ""
-# }
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
